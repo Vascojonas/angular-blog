76a631e10eb19df62f43e95843c618debf2b574b
Vasco Jonas Mabui <vascojonas.j@gmail.com>
2023-02-20 17:07:35 +0200

    content page created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake =[
+    {
+        "id":1,
+        "photo":"https://insider-gaming.com/wp-content/uploads/2023/02/ea-marvel-iron-man.jpg",
+        "title":"Novo lançamento do filme homem de ferro",
+        "description":"Homem de ferro voltou em grande",
+    },
+    {
+        "id":2,
+        "photo":"https://cdn.marvel.com/content/1x/002irm_ons_mas_mob_01_0.jpg",
+        "title":"Tony stark",
+        "description":"O Homem de Ferro foi um personagem dos quadrinhos publicados pela Marvel Comics. Sua verdadeira identidade é o empresário e bilionário Tony Stark, que usou armaduras de alta tecnologia no combate ao crime. Foi criado em 1963 pelo escritor Stan Lee, o roteirista Larry Lieber e os desenhistas Jack Kirby e Don Heck",
+    },
+]
